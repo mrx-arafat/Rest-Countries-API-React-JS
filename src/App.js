0107ebb1d0b3c1ed6462bad9e782f2bdf493ba9d
Easin Arafat <arafatmrx@gmail.com>
2021-09-21 10:04:58 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      <Country></Country>
+      <Countries></Countries>
     </div>
   );
 }
 
-function Country() {
-  const [country, setCountry] = useState([]);
+function Countries() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetch("https://restcountries.eu/rest/v2/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
 
   return (
     <div>
       <h1>All Countries are Here</h1>
+      <h3>Total Countries: {countries.length}</h3>
+
+      {countries.map((country) => (
+        <Country name={country.name}></Country>
+      ))}
+    </div>
+  );
+}
+
+function Country(props) {
+  return (
+    <div>
+      <h2>
+        name:
+        {props.name}
+      </h2>
+      <h3>capital:</h3>
     </div>
   );
 }
