42b656d84b8d5cd25a7938a3efbaa6f5ecf63a84
Easin Arafat <arafatmrx@gmail.com>
2021-09-21 09:11:32 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Country></Country>
+    </div>
+  );
+}
+
+function Country() {
+  const [country, setCountry] = useState([]);
+
+  return (
+    <div>
+      <h1>All Countries are Here</h1>
     </div>
   );
 }
