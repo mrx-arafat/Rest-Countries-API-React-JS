3356f4680daf13a5983ed2a8d5394652a8b496af
Easin Arafat <arafatmrx@gmail.com>
2021-09-22 08:05:42 +0600

    init

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -3,11 +3,14 @@ import React from "react";
 import "./country.css";
 const Country = (props) => {
   console.log(props.country);
-  // Countries er moddhe full country re pathay dichi tai
+  // Countries er moddhe full country re pathay dichi tai.. arr dob kisu props.country tei ache
   return (
     <div className="country">
-      <h3>country name : {props.name}</h3>
-      <h4>country capital : {props.capital}</h4>
+      <img src={props.country.flag} alt="" />
+      <h3>Country name : {props.name}</h3>
+      <h4>Capital : {props.capital}</h4>
+      <h4>Population : {props.country.population}</h4>
+      {/* population : Countries > Country tag e declar kora nei but kaj hoiteche cz country={country} eksathe sobay re call korche ..er moddhei sob ache */}
     </div>
   );
 };
