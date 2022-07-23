0b8237dd88f2f3f57bb3425acb86912eb0bcaadd
mrx-arafat <arafatmrx@gmail.com>
2022-07-23 10:00:05 +0600

    all countries

--- recap rest with js/countries.js
+++ recap rest with js/countries.js
@@ -3,15 +3,22 @@ const loadCountries = () => {
     .then((res) => res.json())
     .then((data) => displayCountries(data));
 };
+//displayCountry->countries parameter e sob data pathai dilam
 
 const displayCountries = (countries) => {
+  //for test
   console.log(countries[0]);
 
+  //countries er upore map kortechi jate prottekta element ke alada kore dhorte pari
+  // countryHTML kintu ekta array return korbe
   const countryHTML = countries.map((country) => getCountryHTML(country));
-
+  //innerHTML  e data set kortechi
   const containerDiv = document.getElementById("container");
-  containerDiv.innerHTML = countryHTML[0];
+  // ekta dekhabar jonno simple but sobgula dekhate gele array element gula join kora lagbe
   console.log(countryHTML[0]);
+  console.log(countryHTML.join(" "));
+  //containerDiv.innerHTML = countryHTML[0];
+  containerDiv.innerHTML = countryHTML.join(" ");
 };
 
 const getCountryHTML = (country) => {
