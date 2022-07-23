579709a13b9300a87101ef9eb78407c0e70038ca
mrx-arafat <arafatmrx@gmail.com>
2022-07-23 09:14:44 +0600

    load in function

--- recap rest with js/countries.js
+++ recap rest with js/countries.js
@@ -1,8 +1,11 @@
 const loadCountries = () => {
   fetch("https://restcountries.com/v3.1/all")
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => displayCountries(data));
+};
+
+const displayCountries = (countries) => {
+  console.log(countries);
 };
 
 loadCountries();
-console.log("oii");
